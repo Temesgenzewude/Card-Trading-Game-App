cb563c8070db9d5cbd48f4dda526b99a6a077a45
Temesgen Zewude <temesgenzewude21@gmail.com>
2023-12-09 10:34:00 +0300

    implement browse set screen

--- card_trading_game_app/lib/src/constants/colors.dart
+++ card_trading_game_app/lib/src/constants/colors.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
+<<<<<<< HEAD
   // Button start and end color
   static const Color buttonStartColor = Color(0xFF3F2E3E);
   static const Color buttonEndColor = Color(0xFFA78295);
 
   // Button gradient color
+=======
+  static const Color buttonStartColor = Color(0xFF3F2E3E);
+  static const Color buttonEndColor = Color(0xFFA78295);
+
+>>>>>>> 8b06ae2 (implement browse set screen)
   static const LinearGradient buttonGradient = LinearGradient(
     begin: Alignment.topCenter,
     end: Alignment.bottomCenter,
     colors: [buttonStartColor, buttonEndColor],
   );
+<<<<<<< HEAD
 
   static const Color primaryColor = Color.fromARGB(255, 63, 46, 62);
   static const Color secondaryColor = Colors.white;
   static const Color borderColor = Color.fromARGB(146, 63, 46, 62);
   static Color shadowColor = Colors.black.withOpacity(0.5);
+=======
+>>>>>>> 8b06ae2 (implement browse set screen)
 }
