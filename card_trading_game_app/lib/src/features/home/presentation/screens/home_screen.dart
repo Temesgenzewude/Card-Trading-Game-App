2cec2ecbc0513615dcb265dc432a888bdc970e17
Temesgen Zewude <temesgenzewude21@gmail.com>
2023-12-09 22:28:46 +0300

    implement more options screen

--- card_trading_game_app/lib/src/features/home/presentation/screens/home_screen.dart
+++ card_trading_game_app/lib/src/features/home/presentation/screens/home_screen.dart
@@ -61,84 +61,10 @@ class HomeScreen extends HookWidget {
                   ],
                 ),
               ),
-              Padding(
-                padding: EdgeInsets.only(
-                  top: AppSizer.getHeight(context, 34),
-                ),
-                child: Container(
-                  height: AppSizer.getHeight(context, 120),
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).secondaryHeaderColor,
-                  ),
-                  child: Row(children: [
-                    Padding(
-                      padding: EdgeInsets.only(
-                          left: AppSizer.getWidth(context, 175), right: 65),
-                      child: Container(
-                        height: AppSizer.getHeight(context, 80),
-                        width: AppSizer.getWidth(context, 180),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Padding(
-                                padding: EdgeInsets.only(
-                                    left: AppSizer.getWidth(context, 10),
-                                    right: AppSizer.getWidth(
-                                      context,
-                                      15,
-                                    ),
-                                    bottom: AppSizer.getHeight(
-                                      context,
-                                      10,
-                                    )),
-                                child: Image.asset(
-                                  'assets/images/logo_images/pokellector_logo.png',
-                                  height: AppSizer.getHeight(context, 80),
-                                  width: AppSizer.getWidth(context, 40),
-                                ),
-                              ),
-                              DropdownButton(
-                                  // isDense: true,
-                                  underline: Container(),
-                                  padding: EdgeInsets.only(
-                                      left: AppSizer.getWidth(context, 10),
-                                      right: AppSizer.getWidth(context, 10)),
-                                  value: selectedCard.value,
-                                  style: TextStyle(
-                                      color: AppColors.darkReddishTextColor,
-                                      fontSize: AppSizer.getHeight(context, 20),
-                                      fontFamily: 'InterBold'),
-                                  dropdownColor:
-                                      Theme.of(context).colorScheme.secondary,
-
-                                  // Down Arrow Icon
-                                  icon: const Padding(
-                                    padding: EdgeInsets.only(left: 20),
-                                    child: Icon(Icons.arrow_drop_down,
-                                        size: 20,
-                                        color: AppColors.darkPurplishTextColor),
-                                  ),
-                                  items: cardGames.map((String items) {
-                                    return DropdownMenuItem(
-                                      value: items,
-                                      child: Text(items),
-                                    );
-                                  }).toList(),
-                                  onChanged: (value) {
-                                    selectedCard.value = value ?? "";
-                                  })
-                            ]),
-                      ),
-                    ),
-                    BuildTabBarForHomePage(tabController: tabController),
-                  ]),
-                ),
-              ),
+              TabBarContainer(
+                  selectedCard: selectedCard,
+                  cardGames: cardGames,
+                  tabController: tabController),
               Expanded(
                 child: TabBarView(controller: tabController, children: const [
                   HomeTab(),
@@ -148,6 +74,98 @@ class HomeScreen extends HookWidget {
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class TabBarContainer extends StatelessWidget {
+  const TabBarContainer({
+    super.key,
+    required this.selectedCard,
+    required this.cardGames,
+    required this.tabController,
+  });
+
+  final ValueNotifier<String> selectedCard;
+  final List<String> cardGames;
+  final TabController tabController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(
+        top: AppSizer.getHeight(context, 34),
+      ),
+      child: Container(
+        height: AppSizer.getHeight(context, 120),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Theme.of(context).secondaryHeaderColor,
+        ),
+        child: Row(children: [
+          Padding(
+            padding: EdgeInsets.only(
+                left: AppSizer.getWidth(context, 175), right: 65),
+            child: Container(
+              height: AppSizer.getHeight(context, 80),
+              width: AppSizer.getWidth(context, 180),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child:
+                  Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+                Padding(
+                  padding: EdgeInsets.only(
+                      left: AppSizer.getWidth(context, 10),
+                      right: AppSizer.getWidth(
+                        context,
+                        15,
+                      ),
+                      bottom: AppSizer.getHeight(
+                        context,
+                        10,
+                      )),
+                  child: Image.asset(
+                    'assets/images/logo_images/pokellector_logo.png',
+                    height: AppSizer.getHeight(context, 80),
+                    width: AppSizer.getWidth(context, 40),
+                  ),
+                ),
+                DropdownButton(
+                    // isDense: true,
+                    underline: Container(),
+                    padding: EdgeInsets.only(
+                        left: AppSizer.getWidth(context, 10),
+                        right: AppSizer.getWidth(context, 10)),
+                    value: selectedCard.value,
+                    style: TextStyle(
+                        color: AppColors.darkReddishTextColor,
+                        fontSize: AppSizer.getHeight(context, 20),
+                        fontFamily: 'InterBold'),
+                    dropdownColor: Theme.of(context).colorScheme.secondary,
+
+                    // Down Arrow Icon
+                    icon: const Padding(
+                      padding: EdgeInsets.only(left: 20),
+                      child: Icon(Icons.arrow_drop_down,
+                          size: 20, color: AppColors.darkPurplishTextColor),
+                    ),
+                    items: cardGames.map((String items) {
+                      return DropdownMenuItem(
+                        value: items,
+                        child: Text(items),
+                      );
+                    }).toList(),
+                    onChanged: (value) {
+                      selectedCard.value = value ?? "";
+                    })
+              ]),
+            ),
+          ),
+          BuildTabBarForHomePage(tabController: tabController),
+        ]),
       ),
     );
   }
