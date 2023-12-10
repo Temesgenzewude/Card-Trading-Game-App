7b46eed36531c53546f465dfd9e6fe8ef83fc712
Temesgen Zewude <temesgenzewude21@gmail.com>
2023-12-10 15:15:11 +0300

    update the tab container

--- card_trading_game_app/lib/src/features/home/presentation/widgets/build_tab_bar_container.dart
+++ card_trading_game_app/lib/src/features/home/presentation/widgets/build_tab_bar_container.dart
@@ -25,56 +25,64 @@ class TabBarContainer extends StatelessWidget {
       child: Container(
         height: AppSizer.getHeight(context, 84),
         width: double.infinity,
+        padding: EdgeInsets.only(
+          right: AppSizer.getWidth(context, 165),
+        ),
         decoration: BoxDecoration(
           color: Theme.of(context).secondaryHeaderColor,
         ),
-        child: Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
-          Padding(
-            padding: EdgeInsets.only(
-                left: AppSizer.getWidth(context, 155), right: 21),
-            child: Container(
-              width: AppSizer.getWidth(context, 100),
-              height: AppSizer.getHeight(context, 84),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                gradient: const LinearGradient(
-                  begin: Alignment.centerLeft,
-                  end: Alignment.centerRight,
-                  colors: [Color(0xFF8ECAE6), Color(0xFF219EBC)],
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.25),
-                    offset: const Offset(0, 6),
-                    blurRadius: 8,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+              Padding(
+                padding: EdgeInsets.only(
+                    left: AppSizer.getWidth(context, 155)
+                    , right: 21),
+                child: Container(
+                  width: AppSizer.getWidth(context, 100),
+                  height: AppSizer.getHeight(context, 84),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    gradient: const LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      colors: [Color(0xFF8ECAE6), Color(0xFF219EBC)],
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.25),
+                        offset: const Offset(0, 6),
+                        blurRadius: 8,
+                      ),
+                    ],
                   ),
-                ],
+                  child: Center(
+                    child: Image.asset(
+                        'assets/images/logo_images/pokellector_logo.png',
+                        width: AppSizer.getWidth(context, 86),
+                        height: AppSizer.getHeight(context, 31)),
+                  ),
+                ),
               ),
-              child: Center(
+              Image.asset(
+                'assets/images/logo_images/one_piece__logo.png',
+                width: AppSizer.getWidth(context, 97),
+                height: AppSizer.getHeight(context, 25),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
                 child: Image.asset(
-                    'assets/images/logo_images/pokellector_logo.png',
-                    width: AppSizer.getWidth(context, 86),
-                    height: AppSizer.getHeight(context, 31)),
+                  'assets/images/logo_images/disney_lorcana_logo.png',
+                  width: AppSizer.getWidth(context, 70),
+                  height: AppSizer.getHeight(context, 31),
+                ),
               ),
-            ),
-          ),
-          Image.asset(
-            'assets/images/logo_images/one_piece__logo.png',
-            width: AppSizer.getWidth(context, 97),
-            height: AppSizer.getHeight(context, 25),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 20),
-            child: Image.asset(
-              'assets/images/logo_images/disney_lorcana_logo.png',
-              width: AppSizer.getWidth(context, 70),
-              height: AppSizer.getHeight(context, 31),
-            ),
-          ),
-          BuildTabBarForHomePage(tabController: tabController),
-        ]),
+            ]),
+            BuildTabBarForHomePage(tabController: tabController),
+          ],
+        ),
       ),
     );
   }
 }
- 
