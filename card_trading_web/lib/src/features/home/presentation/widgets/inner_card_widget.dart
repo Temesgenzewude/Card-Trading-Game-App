6e5b90dc2b2e667eea06e5240e5ac3b2b88adb9b
Stargazing-11 <samuelkifle199314@gmail.com>
2023-12-13 15:36:36 +0300

    update home screen tab

--- card_trading_web/lib/src/features/home/presentation/widgets/inner_card_widget.dart
+++ card_trading_web/lib/src/features/home/presentation/widgets/inner_card_widget.dart
@@ -16,43 +16,46 @@ class InnerCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(
-        horizontal: AppSizer.getWidth(context, 10),
-        vertical: AppSizer.getHeight(context, 12),
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-        color: Theme.of(context).colorScheme.secondary,
-        boxShadow: [
-          BoxShadow(
-            color: Theme.of(context).colorScheme.shadow,
-            offset: const Offset(0, 0),
-            blurRadius: 1,
-            // spreadRadius: 1,
-          ),
-        ],
-      ),
-      child: Row(
-        children: [
-          Image(
-            image: AssetImage(imageUrl),
-            width: AppSizer.getWidth(context, 33),
-            fit: BoxFit.cover,
-          ),
-          SizedBox(
-            width: AppSizer.getWidth(context, 12),
-          ),
-          Flexible(
-            child: Text(
-              title,
-              style: TextStyle(
-                color: Theme.of(context).colorScheme.primary,
-                fontSize: 15,
-              ),
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: AppSizer.getWidth(context, 10),
+          vertical: AppSizer.getHeight(context, 12),
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: Theme.of(context).colorScheme.secondary,
+          boxShadow: [
+            BoxShadow(
+              color: Theme.of(context).colorScheme.shadow,
+              offset: const Offset(0, 0),
+              blurRadius: 1,
+              // spreadRadius: 1,
             ),
-          )
-        ],
+          ],
+        ),
+        child: Row(
+          children: [
+            Image(
+              image: AssetImage(imageUrl),
+              width: AppSizer.getWidth(context, 33),
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              width: AppSizer.getWidth(context, 12),
+            ),
+            Flexible(
+              child: Text(
+                title,
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontSize: 15,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
