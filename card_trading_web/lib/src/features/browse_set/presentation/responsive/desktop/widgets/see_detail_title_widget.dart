61c2babf85ff5bce292fe146f755b467d2ce2930
Stargazing-11 <samuelkifle199314@gmail.com>
2023-12-13 15:21:15 +0300

    update see detail title widget

--- card_trading_web/lib/src/features/browse_set/presentation/responsive/desktop/widgets/see_detail_title_widget.dart
+++ card_trading_web/lib/src/features/browse_set/presentation/responsive/desktop/widgets/see_detail_title_widget.dart
@@ -1,3 +1,4 @@
+import 'package:card_trading_web/src/common_widgets/box_shadow.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../../../utils/size_convertor.dart';
@@ -18,14 +19,7 @@ class SeeDetailTitleWidget extends StatelessWidget {
             padding: const EdgeInsets.all(20),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(3),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.25),
-                  offset: const Offset(0, 4),
-                  blurRadius: 4,
-                  spreadRadius: 1,
-                ),
-              ],
+              boxShadow: [boxShadow()],
               border: Border.all(
                 color: Theme.of(context).colorScheme.primary.withOpacity(0.8),
                 width: 1,
@@ -52,15 +46,17 @@ class SeeDetailTitleWidget extends StatelessWidget {
               child: Text(
                 'SetName',
                 style: TextStyle(
-                  fontSize: AppSizer.getHeight(context, 20),
+                  fontSize: 16,
                   fontWeight: FontWeight.bold,
                   color: Theme.of(context).colorScheme.onTertiary,
                 ),
               ),
             ),
-            const Padding(
-              padding: EdgeInsets.only(bottom: 40.0),
-              child: Text('The first TCG set to feature Paradox Pokemon'),
+            Padding(
+              padding: EdgeInsets.only(
+                bottom: AppSizer.getHeight(context, 36),
+              ),
+              child: const Text('The first TCG set to feature Paradox Pokemon'),
             ),
             const Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
