72707b7b2064f9eea09c4007cc7c3c84167c6043
Temesgen Zewude <temesgenzewude21@gmail.com>
2023-12-13 17:24:15 +0300

    add url Path Strategy

--- card_trading_web/lib/main.dart
+++ card_trading_web/lib/main.dart
@@ -1,12 +1,16 @@
-import 'package:card_trading_web/src/shared_pref/shared_pref_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'package:card_trading_web/src/shared_pref/shared_pref_manager.dart';
 
 import 'dependency_injection/shared_pref_injection.dart';
 import 'src/routing/main_router.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  usePathUrlStrategy();
 
   await initPrefManager();
 
