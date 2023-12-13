68240f14199d29073e7cc29522363cb0b7fbeba4
Stargazing-11 <samuelkifle199314@gmail.com>
2023-12-13 14:28:20 +0300

    add inkwell to lightbutton widget

--- card_trading_web/lib/src/features/browse_set/presentation/responsive/desktop/widgets/light_button_widget.dart
+++ card_trading_web/lib/src/features/browse_set/presentation/responsive/desktop/widgets/light_button_widget.dart
@@ -8,22 +8,25 @@ class LightButtonWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(
-        horizontal: AppSizer.getWidth(context, 34),
-        vertical: AppSizer.getHeight(context, 14),
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(32),
-        color: const Color.fromARGB(255, 142, 202, 230),
-      ),
-      child: FittedBox(
-        fit: BoxFit.scaleDown,
-        child: Text(
-          'Complete your collection',
-          style: TextStyle(
-            fontSize: 12,
-            color: Theme.of(context).colorScheme.secondary.withOpacity(0.5),
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: AppSizer.getWidth(context, 34),
+          vertical: AppSizer.getHeight(context, 14),
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(32),
+          color: const Color.fromARGB(255, 142, 202, 230),
+        ),
+        child: FittedBox(
+          fit: BoxFit.scaleDown,
+          child: Text(
+            'Complete your collection',
+            style: TextStyle(
+              fontSize: 12,
+              color: Theme.of(context).colorScheme.secondary.withOpacity(0.5),
+            ),
           ),
         ),
       ),
