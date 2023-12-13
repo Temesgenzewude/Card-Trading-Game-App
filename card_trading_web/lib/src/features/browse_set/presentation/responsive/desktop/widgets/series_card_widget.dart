429cf6f403e43a0b357837d26a15be3ae1f681b7
Stargazing-11 <samuelkifle199314@gmail.com>
2023-12-13 15:31:35 +0300

    update series card widgets

--- card_trading_web/lib/src/features/browse_set/presentation/responsive/desktop/widgets/series_card_widget.dart
+++ card_trading_web/lib/src/features/browse_set/presentation/responsive/desktop/widgets/series_card_widget.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: must_be_immutable
 
+import 'package:card_trading_web/src/common_widgets/box_shadow.dart';
+import 'package:card_trading_web/src/utils/size_convertor.dart';
 import 'package:flutter/material.dart';
-
-import '../../../../../../utils/size_convertor.dart';
 
 class SeriesCardWidget extends StatelessWidget {
   SeriesCardWidget({super.key, this.imageWidth});
@@ -11,35 +11,33 @@ class SeriesCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(
-        vertical: 15,
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(3),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.25),
-            offset: const Offset(0, 4),
-            blurRadius: 4,
-            spreadRadius: 1,
-          ),
-        ],
-        border: Border.all(
-          color: Theme.of(context).colorScheme.primary.withOpacity(0.8),
-          width: 1,
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        padding: const EdgeInsets.symmetric(
+          vertical: 15,
         ),
-        color: Theme.of(context).colorScheme.secondary,
-      ),
-      child: Column(
-        children: [
-          Image.asset(
-            'assets/images/image26.png',
-            width: AppSizer.getWidth(context, imageWidth ?? 115),
-            fit: BoxFit.cover,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(3),
+          boxShadow: [
+            boxShadow(),
+          ],
+          border: Border.all(
+            color: Theme.of(context).colorScheme.primary.withOpacity(0.8),
+            width: 1,
           ),
-          const Text('set name'),
-        ],
+          color: Theme.of(context).colorScheme.secondary,
+        ),
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/images/image26.png',
+              width: AppSizer.getWidth(context, imageWidth ?? 115),
+              fit: BoxFit.cover,
+            ),
+            const Text('set name'),
+          ],
+        ),
       ),
     );
   }
