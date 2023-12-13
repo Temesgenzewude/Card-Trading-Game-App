62f5bbd08d15275ca728c4815a83a9c40383e65b
Stargazing-11 <samuelkifle199314@gmail.com>
2023-12-13 14:25:12 +0300

    add inkwell to product card widget

--- card_trading_web/lib/src/features/home/presentation/widgets/product_card_widget.dart
+++ card_trading_web/lib/src/features/home/presentation/widgets/product_card_widget.dart
@@ -1,7 +1,6 @@
+import 'package:card_trading_web/src/constants/icons.dart';
+import 'package:card_trading_web/src/utils/size_convertor.dart';
 import 'package:flutter/material.dart';
-
-import '../../../../constants/icons.dart';
-import '../../../../utils/size_convertor.dart';
 
 class ProductCardWidget extends StatelessWidget {
   const ProductCardWidget({
@@ -10,55 +9,59 @@ class ProductCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: AppSizer.getWidth(context, 87),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Image(
-            width: AppSizer.getWidth(context, 87),
-            // height: AppSizer.getHeight(context, 121),
-            fit: BoxFit.cover,
-            image: const AssetImage(
-              'assets/images/image14.png',
-            ),
-          ),
-          Wrap(
-            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              const Row(
-                children: [
-                  AppIcons.yellowStar,
-                  AppIcons.yellowStar,
-                  AppIcons.yellowStar,
-                  AppIcons.yellowStar,
-                  AppIcons.yellowStar,
-                ],
+    return InkWell(
+      onTap: () {},
+      child: SizedBox(
+        width: AppSizer.getWidth(context, 87),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image(
+              width: AppSizer.getWidth(context, 87),
+              // height: AppSizer.getHeight(context, 121),
+              fit: BoxFit.cover,
+              image: const AssetImage(
+                'assets/images/image14.png',
               ),
-              Text(
-                '(512)',
-                style: TextStyle(
-                    fontSize: 10, color: Theme.of(context).colorScheme.primary),
-              )
-            ],
-          ),
-          Text(
-            'Card Name',
-            style: TextStyle(
-              fontSize: 12,
-              color: Theme.of(context).colorScheme.primary,
             ),
-          ),
-          Text(
-            '\$0.85',
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.bold,
-              color: Theme.of(context).colorScheme.primary,
+            Wrap(
+              // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const Row(
+                  children: [
+                    AppIcons.yellowStar,
+                    AppIcons.yellowStar,
+                    AppIcons.yellowStar,
+                    AppIcons.yellowStar,
+                    AppIcons.yellowStar,
+                  ],
+                ),
+                Text(
+                  '(512)',
+                  style: TextStyle(
+                      fontSize: 10,
+                      color: Theme.of(context).colorScheme.primary),
+                )
+              ],
             ),
-          )
-        ],
+            Text(
+              'Card Name',
+              style: TextStyle(
+                fontSize: 12,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            ),
+            Text(
+              '\$0.85',
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
