29e3d87a52acfba86e015678a329fb9bd12bd755
Stargazing-11 <samuelkifle199314@gmail.com>
2023-12-14 15:32:34 +0300

    add onhover effect on see card detail view widget

---
+++ card_trading_web/lib/src/features/browse_set/presentation/responsive/desktop/widgets/card_collection_item.dart
@@ -0,0 +1,46 @@
+
+// ignore_for_file: library_private_types_in_public_api, must_be_immutable
+
+import 'package:card_trading_web/src/features/browse_set/presentation/responsive/desktop/widgets/hover_content_widget.dart';
+import 'package:card_trading_web/src/utils/size_convertor.dart';
+import 'package:flutter/material.dart';
+
+class CardCollectionItem extends StatefulWidget {
+  CardCollectionItem({super.key, required this.index});
+  int index;
+
+  @override
+  _CardCollectionItemState createState() => _CardCollectionItemState();
+}
+
+class _CardCollectionItemState extends State<CardCollectionItem> {
+  bool isHovered = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      onEnter: (_) => setState(() => isHovered = true),
+      onExit: (_) => setState(() => isHovered = false),
+      child: Container(
+        width: AppSizer.getWidth(context, 140),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(3),
+          color: Theme.of(context).colorScheme.secondary,
+        ),
+        child: Stack(
+          children: [
+            Image.asset(
+              'assets/images/image3.png',
+              fit: BoxFit.cover,
+            ),
+            if (isHovered)
+              HoverContent(
+                index: widget.index,
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
