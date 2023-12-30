474b0bf8e717d02e26e00c3fcf302f422878788d
Temesgen Zewude <temesgenzewude21@gmail.com>
2023-12-30 12:19:44 +0300

    add alert dialog for search bar

--- card_trading_web/lib/src/common_widgets/primary_search_bar_widget.dart
+++ card_trading_web/lib/src/common_widgets/primary_search_bar_widget.dart
@@ -134,10 +134,12 @@ class _MyDialogState extends State<MyDialog> {
   @override
   Widget build(BuildContext context) {
     return Dialog(
+      insetPadding: const EdgeInsets.only(bottom: 450, left: 0, right: 70),
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
       child: Container(
         width: AppSizer.getWidth(context, 400),
-        height: AppSizer.getHeight(context, 500),
+        // height: AppSizer.getHeight(context, 500),
+        padding: const EdgeInsets.only(bottom: 10),
         decoration: ShapeDecoration(
           color: Colors.white,
           shape: RoundedRectangleBorder(
@@ -239,31 +241,171 @@ class _MyDialogState extends State<MyDialog> {
                 ),
               ),
             ),
-            const SizedBox(height: 16),
-            CheckboxListTile(
-              title: const Text('Set Name'),
-              value: _setNameChecked,
-              onChanged: (value) => setState(() => _setNameChecked = value!),
-            ),
-            CheckboxListTile(
-              title: const Text('Card Name'),
-              value: _cardNameChecked,
-              onChanged: (value) => setState(() => _cardNameChecked = value!),
-            ),
-            const SizedBox(height: 16),
+            const SizedBox(height: 10),
             Row(
-              mainAxisAlignment: MainAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                TextButton(
-                  onPressed: _clearFilters,
-                  child: const Text('Clear Filters'),
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 70),
+                      child: Checkbox(
+                        activeColor: Colors.green,
+                        checkColor: Colors.white,
+                        value: _setNameChecked,
+                        onChanged: (value) =>
+                            setState(() => _setNameChecked = value!),
+                      ),
+                    ),
+                    const Padding(
+                        padding: EdgeInsets.only(left: 10),
+                        child: Text(
+                          'Set name',
+                          style: TextStyle(
+                            color: Color(0xFF3F2E3E),
+                            fontSize: 12,
+                            fontFamily: 'InterRegular',
+                            fontWeight: FontWeight.w400,
+                            height: 0,
+                          ),
+                        )),
+                  ],
                 ),
-                const SizedBox(width: 8),
-                ElevatedButton(
-                  onPressed: _startSearching,
-                  child: const Text('Start Searching'),
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 60),
+                      child: Checkbox(
+                        activeColor: Colors.green,
+                        checkColor: Colors.white,
+                        value: _cardNameChecked,
+                        onChanged: (value) =>
+                            setState(() => _cardNameChecked = value!),
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 10),
+                      child: Text(
+                        'Card name',
+                        style: TextStyle(
+                          color: Color(0xFF3F2E3E),
+                          fontSize: 12,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w400,
+                          height: 0,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 70, vertical: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  InkWell(
+                    onTap: _clearFilters,
+                    child: Container(
+                      width: 115,
+                      height: 43,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 14, vertical: 12),
+                      clipBehavior: Clip.antiAlias,
+                      decoration: ShapeDecoration(
+                        color: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          side: const BorderSide(
+                              width: 1, color: Color(0xFF8ECAE6)),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        shadows: const [
+                          BoxShadow(
+                            color: Color(0x3F000000),
+                            blurRadius: 8,
+                            offset: Offset(0, 6),
+                            spreadRadius: 0,
+                          )
+                        ],
+                      ),
+                      child: const Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Clear filters',
+                            style: TextStyle(
+                              color: Color(0xFF219EBC),
+                              fontSize: 16,
+                              fontFamily: 'Inter',
+                              fontWeight: FontWeight.w400,
+                              height: 0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  InkWell(
+                    onTap: _startSearching,
+                    child: Container(
+                      width: 83,
+                      height: 43,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 14, vertical: 12),
+                      clipBehavior: Clip.antiAlias,
+                      decoration: ShapeDecoration(
+                        gradient: const LinearGradient(
+                          begin: Alignment(0.00, -1.00),
+                          end: Alignment(0, 1),
+                          colors: [Color(0xFF8ECAE6), Color(0xFF219EBC)],
+                        ),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8)),
+                        shadows: const [
+                          BoxShadow(
+                            color: Color(0x3F000000),
+                            blurRadius: 8,
+                            offset: Offset(0, 6),
+                            spreadRadius: 0,
+                          )
+                        ],
+                      ),
+                      child: const Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Search',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontFamily: 'Inter',
+                              fontWeight: FontWeight.w700,
+                              height: 0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  // ElevatedButton(
+                  //   onPressed: _startSearching,
+                  //   style: ElevatedButton.styleFrom(
+                  //       backgroundColor: Colors.transparent,
+                  //       shadowColor: Colors.transparent,
+
+                  //       shape: RoundedRectangleBorder(
+                  //         borderRadius: BorderRadius.circular(40),
+                  //       )),
+                  //   child: const Text('Start Searching'),
+                  // ),
+                ],
+              ),
             ),
           ],
         ),
